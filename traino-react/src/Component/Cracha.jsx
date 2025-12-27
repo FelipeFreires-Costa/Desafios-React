5be3d2f7d6a5c12a1a1d1bb7c9f14094502cd5a1
Felipe Freires <felipeplayf13@gmail.com>
2025-12-27 11:06:04 -0300

    Aprimorando aprendizado de useState

--- traino-react/src/Component/Cracha.jsx
+++ traino-react/src/Component/Cracha.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import { useState } from 'react'
 
 //fazer desestruturaçao, usar menos props e usar mais as variaveis
 const Cracha = ({nome, cargo, cor}) => {
+
+  const [estaConectado, setEstaConectado] = useState(false)
+  const [numeroLikes, setNumeroLikes] = useState(0)
+
+  function contadorLike(){
+    setNumeroLikes(numeroLikes + 1)
+  }
+
   function handleConexao(){
-    alert(`Solicitação enviada para o ${nome}`)
+    setEstaConectado(!estaConectado)
   }
 
   return (
     <div>
       <h2>{nome}</h2>
       <p>{cargo}</p>
-      <button onClick={handleConexao} style={{backgroundColor: cor}} >Conectar</button>
+      <button onClick={contadorLike}>❤️ {numeroLikes}</button>
+      <button onClick={handleConexao} style={{backgroundColor: cor}} >{estaConectado ? "Conectado" : "Conectar"}</button>
     </div>
   )
 }
