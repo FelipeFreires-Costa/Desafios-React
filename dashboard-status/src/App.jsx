26b963562bb887e2bc67dad49ad2a4b20cc4bb1d
Felipe Freires <felipeplayf13@gmail.com>
2026-01-02 12:55:45 -0300

    praticando .map e .filter

--- dashboard-status/src/App.jsx
+++ dashboard-status/src/App.jsx
@@ -6,27 +6,36 @@ function App() {
 const [servidores, setServidores] = useState([
     { id: 1, nome: "Servidor AWS", ip: "192.168.1.1", status: "online" },
     { id: 2, nome: "Servidor Google", ip: "192.168.1.2", status: "offline" },
-    { id: 3, nome: "Banco de Dados", ip: "192.168.1.5", status: "alerta" },
+    { id: 3, nome: "Banco de Dados", ip: "192.168.1.5", status: "online" },
     { id: 4, nome: "Servidor de Email", ip: "192.168.1.9", status: "online" }
   ]);
 
   const [filtro, setFiltro] = useState("todos")
-
-  let listaFiltrada = servidores.filter((servidor) => {
+  
+  
+    let listaFiltrada = servidores.filter((servidor) => {
     if(filtro === "todos") return true
     return servidor.status === filtro
   })
+  
+  let conteudoPrincipal;
+  
+  if(listaFiltrada.length === 0){
+    conteudoPrincipal = <p>nenhum servidor encontrado</p>
+  } else {
+    conteudoPrincipal = listaFiltrada.map(item => <CardServidor key={item.id} {...item}/>)
+  }
 
   return (
   <div>
     <button onClick={() => setFiltro("todos")}>Todos</button>
+    <button onClick={() => setFiltro("alerta")}>Alerta</button>
     <button onClick={() => setFiltro("online")}>Online</button>
     <button onClick={() => setFiltro("offline")}>Offline</button>
-    {
-      listaFiltrada.map((item) =>(
-        <CardServidor key={item.id} nome={item.nome} ip={item.ip}  status={item.status}/>
-      ))
-    }
+    
+    <div>
+    {conteudoPrincipal}
+    </div>
   </div>
 )
 }
