ca1e4983c6dba213bac7b07ab037f475eb85ad6e
Felipe Freires <felipeplayf13@gmail.com>
2026-01-21 11:04:16 -0300

    Crud completo

--- CRUD-estudo/src/App.jsx
+++ CRUD-estudo/src/App.jsx
@@ -1,70 +1,114 @@
-import { useState } from 'react'
-import './App.css'
-import CardProdutos from './Components/CardProdutos'
+import { useEffect, useState } from "react";
+import "./App.css";
+import CardProdutos from "./Components/CardProdutos";
 
 function App() {
-
   const [produtos, setProdutos] = useState([
-    {id: 1, nome: "notebook", preco: 2500},
-    {id: 2, nome: "Mouse", preco: 90},
-    {id: 3, nome: "Teclado", preco: 200},
-    {id: 4, nome: "Mouse pad", preco: 50}
-  ])
+    { id: 1, nome: "notebook", preco: 2500 },
+    { id: 2, nome: "Mouse", preco: 90 },
+    { id: 3, nome: "Teclado", preco: 200 },
+    { id: 4, nome: "Mouse pad", preco: 50 },
+  ]);
 
-  const [nomeInput, setNomeInput] = useState("")
-  const [precoInput, setPrecoInput] = useState("")
+  const [nomeInput, setNomeInput] = useState("");
+  const [precoInput, setPrecoInput] = useState("");
+  const [idEditando, setIdEditando] = useState(null);
 
-  function adicionarProduto(){
-    //validaçao basica (nao deixa salvar vazio)
-    if(nomeInput === "" || precoInput === ""){
-      alert("Preencha todos os campos!")
-      return
-    }
-
-    //criar novo objeto
-    const novoProduto = {
-      id: Date.now(),
-      nome: nomeInput,
-      preco: Number(precoInput) //garante que o preco seja numero
-    }
-
-    setProdutos([...produtos, novoProduto])
-
-    //limpar os inputs
-    setNomeInput("")
-    setPrecoInput("")
-  } 
-
-  function removerProduto(id){
-    const  novalista = (produtos.filter((item) => item.id !== id))
-
-    setProdutos(novalista)
+  function prepararEdicao(produto) {
+    setIdEditando(produto.id);
+    setNomeInput(produto.nome);
+    setPrecoInput(produto.preco);
   }
 
-  console.log(produtos)
+  function salvarProduto() {
+    //validaçao basica (nao deixa salvar vazio)
+    if (nomeInput === "" || precoInput === "") {
+      alert("Preencha todos os campos!");
+      return;
+    }
+
+    //logica EDICAO
+    if (idEditando !== null) {
+      const listaAtualizada = produtos.map((item) => {
+        if (item.id === idEditando) {
+          return { ...item, nome: nomeInput, preco: Number(precoInput) };
+        } else {
+          return item;
+        }
+      });
+      setProdutos(listaAtualizada);
+      setIdEditando(null); //sair do modo ediçao
+    } else {
+      //criar novo objeto
+      const novoProduto = {
+        id: Date.now(),
+        nome: nomeInput,
+        preco: Number(precoInput), //garante que o preco seja numero
+      };
+      setProdutos([...produtos, novoProduto]);
+    }
+    //limpar os inputs
+    setNomeInput("");
+    setPrecoInput("");
+  }
+
+  function removerProduto(id) {
+    const novalista = produtos.filter((item) => item.id !== id);
+
+    setProdutos(novalista);
+  }
+
+  useEffect(() => {
+    console.log("a lista de produtos mudou ", produtos);
+
+  }, [produtos])
 
   return (
     <div>
       <h1>Controle de estoque</h1>
 
-    <div className="formulario" style={{ marginBottom: '20px', padding: '10px', border: '1px dashed #aaa' }}>
+      <div
+        className="formulario"
+        style={{
+          marginBottom: "20px",
+          padding: "10px",
+          border: "1px dashed #aaa",
+        }}
+      >
         <h3>Cadastrar Novo produto</h3>
 
-        <input onChange={(e) => setNomeInput(e.target.value)} value={nomeInput} id="" placeholder='nome do produto'/>
-        <input type="number" onChange={(e) => setPrecoInput(e.target.value)} value={precoInput} placeholder='preço' style={{ marginLeft: '10px' }} />
-        <button onClick={adicionarProduto}>+ adicionar</button>
-    </div>
+        <input
+          onChange={(e) => setNomeInput(e.target.value)}
+          value={nomeInput}
+          id=""
+          placeholder="nome do produto"
+        />
+        <input
+          type="number"
+          onChange={(e) => setPrecoInput(e.target.value)}
+          value={precoInput}
+          placeholder="preço"
+          style={{ marginLeft: "10px" }}
+        />
+
+        <button onClick={salvarProduto}>
+          {idEditando === null ? "Adicionar" : "Salvar alteração"}
+        </button>
+      </div>
 
       {produtos.map((item) => (
-        <CardProdutos key={item.id} nome={item.nome} preco={item.preco} aoRemover={() => removerProduto(item.id)}/>
-      ))
-      }
+        <CardProdutos
+          key={item.id}
+          nome={item.nome}
+          preco={item.preco}
+          aoRemover={() => removerProduto(item.id)}
+          aoEditar={() => prepararEdicao(item)}
+        />
+      ))}
 
-      {
-        produtos.length === 0 && <p>Nenhum produto no estoque.</p>
-      }
+      {produtos.length === 0 && <p>Nenhum produto no estoque.</p>}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- CRUD-estudo/src/Components/CardProdutos.jsx
+++ CRUD-estudo/src/Components/CardProdutos.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-function CardProdutos({ nome, preco, aoRemover }) {
+function CardProdutos({ nome, preco, aoRemover, aoEditar }) {
   return (
     <div>
       <span>
         <strong>{nome}</strong> - R$ {preco.toFixed(2)}
       </span>
       <button style={{ background: 'red', color: 'white', border: 'none', cursor: 'pointer' }} onClick={aoRemover}>Remover</button>
+      <button style={{ background: 'blue', color: 'white', border: 'none', cursor: 'pointer', marginLeft: '10px' }} onClick={aoEditar}>Editar</button>
     </div>
   );
 }
