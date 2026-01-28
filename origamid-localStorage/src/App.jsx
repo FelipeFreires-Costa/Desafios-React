ab9e965079059c17a7371131d8fd8fae78c9f90f
Felipe Freires <felipeplayf13@gmail.com>
2026-01-28 11:33:06 -0300

    preferencia com localStorage

---
+++ origamid-localStorage/src/App.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+import Produtos from './Components/Produtos'
+
+
+function App() {
+
+const [produto, setProduto] = useState(null)
+
+  useEffect(() => {
+    const produtoLocal = window.localStorage.getItem('produto')
+    if(produtoLocal !== null){
+      setProduto(produtoLocal)
+    } 
+  }, [])
+
+  useEffect(() => {
+    if(produto !== null){
+      window.localStorage.setItem('produto', produto)
+    }
+  }, [produto])
+
+
+  function handleClick({target}){
+    setProduto(target.innerText)
+  }
+
+  return (
+    <div>
+      <h1>Preferencia: {produto}</h1>
+      <button onClick={handleClick}>Notebook</button>
+      <button onClick={handleClick}>Smartphone</button>
+
+      <Produtos  produto={produto}/>
+    </div>
+  )
+}
+
+export default App
